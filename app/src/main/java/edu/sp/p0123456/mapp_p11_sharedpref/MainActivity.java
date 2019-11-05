8d27108a16442b05b84682a08d7e71d113179889
Boon Yuen Tan <tan_boon_yuen@ichat.sp.edu.sg>
2019-11-05 08:17:21 +0800

    Initial commit

---
+++ app/src/main/java/edu/sp/p0123456/mapp_p11_sharedpref/MainActivity.java
@@ -0,0 +1,99 @@
+package edu.sp.p0123456.mapp_p11_sharedpref;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Key for current count
+    private final String COUNT_KEY = "count";
+    // Key for current color
+    private final String COLOR_KEY = "color";
+    // Current count.
+    private int mCount = 0;
+    // Current background color.
+    private int mColor;
+    // Text view to display both count and color.
+    private TextView mShowCountTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize views, color
+        mShowCountTextView = (TextView) findViewById(R.id.count_textview);
+        mColor = ContextCompat.getColor(this, R.color.default_background);
+
+        // Restore the saved state. See onSaveInstanceState() for what gets saved.
+        if (savedInstanceState != null) {
+
+            mCount = savedInstanceState.getInt(COUNT_KEY);
+            if (mCount != 0) {
+                mShowCountTextView.setText(String.format("%s", mCount));
+            }
+
+            mColor = savedInstanceState.getInt(COLOR_KEY);
+            mShowCountTextView.setBackgroundColor(mColor);
+        }
+    }
+
+    /**
+     * Handles the onClick for the background color buttons.  Gets background color of the button
+     * that was clicked and sets the textview background to that color.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void changeBackground(View view) {
+        int color = ((ColorDrawable) view.getBackground()).getColor();
+        mShowCountTextView.setBackgroundColor(color);
+        mColor = color;
+    }
+
+    /**
+     * Handles the onClick for the Count button.  Increments the value of the mCount global and
+     * updates the textview.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void countUp(View view) {
+        mCount++;
+        mShowCountTextView.setText(String.format("%s", mCount));
+    }
+
+    /**
+     * Saves the instance state if the activity is restarted (for example, on device rotation.)
+     * Here you save the values for the count and the background color.
+     *
+     * @param outState The state data.
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(COUNT_KEY, mCount);
+        outState.putInt(COLOR_KEY, mColor);
+    }
+
+    /**
+     * Handles the onClick for the Reset button.  Resets the global count and background
+     * variables to the defaults, resets the views to those values, and clears the shared
+     * preferences
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void reset(View view) {
+        // Reset count
+        mCount = 0;
+        mShowCountTextView.setText(String.format("%s", mCount));
+
+        // Reset color
+        mColor = ContextCompat.getColor(this, R.color.default_background);
+        mShowCountTextView.setBackgroundColor(mColor);
+    }
+}
